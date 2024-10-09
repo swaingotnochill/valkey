c1e1caa8fd4940c7093786e001d9d7f4d4a8960d
Roshan Swain <swainroshan001@gmail.com>
2024-10-09 22:23:14 +0530

    feat: adding fast_float dependency header file

---
+++ deps/fast_float/fast_float_strtod.cpp
@@ -0,0 +1,13 @@
+/*
+ * fast_float_wrapper.cpp - This is a C wrapper for the fast_float library.
+ * Author: Roshan Swain <swainroshan001@gmail.com>
+ */
+
+#include "fast_float_strtod.h"
+#include "fast_float.h"
+
+extern "C" {
+double fast_float_strtod(const char *nptr, char **endptr) {
+    // TODO: Complete this.
+   }
+}
